322ae548125800f3e198e0a5ef36c4b905873919
jkl <189156395@qq.com>
2020-12-20 00:47:14 +0800

    项目基础骨架搭建完成

---
+++ tencent_demo/tencent_demo/libs/__init__.py
@@ -0,0 +1,6 @@
+# @Author  : jkl
+# @Email   : 189156395@qq.com
+# @Time    : 2020/12/20 上午12:13
+# @File    : __init__.py.py
+
+
---
+++ tencent_demo/tencent_demo/settings/__init__.py
@@ -0,0 +1,6 @@
+# @Author  : jkl
+# @Email   : 189156395@qq.com
+# @Time    : 2020/12/20 上午12:14
+# @File    : __init__.py.py
+
+
